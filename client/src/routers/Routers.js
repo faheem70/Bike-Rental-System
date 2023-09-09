f9dd42324e38932470a8224140973593f7e75314
Faheem Akhtar <faheemakhtar19730@gmail.com>
2023-09-09 16:36:38 +0530

    update

--- client/src/routers/Routers.js
+++ client/src/routers/Routers.js
@@ -1,10 +1,10 @@
-import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-
-// Import your components
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "../pages/Home";
 import About from "../pages/About";
 import CarListing from "../pages/CarListing";
 import CarDetails from "../pages/CarDetails";
+
 import NotFound from "../pages/NotFound";
 import Contact from "../pages/Contact";
 import Login from "../pages/Login";
@@ -13,24 +13,23 @@ import Otp from "../pages/Otp";
 import CarProduct from "../components/UI/CarProduct";
 import Checkout from "../pages/Checkout";
 
+
 const Routers = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Navigate to="/home" />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/cars" element={<CarListing />} />
-        <Route path="/cars/:slug" element={<CarDetails />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="*" element={<NotFound />} />
-        <Route path="/checkout" element={<Checkout />} />
-        <Route path='/user/otp' element={<Otp />} />
-        <Route path="/cartproduct" element={<CarProduct />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Navigate to="/home" />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/cars" element={<CarListing />} />
+      <Route path="/cars/:slug" element={<CarDetails />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="*" element={<NotFound />} />
+      <Route path="/checkout" element={<Checkout />} />
+      <Route path='/user/otp' element={<Otp />} />
+      <Route path="/cartproduct" element={<CarProduct />} />
+    </Routes>
   );
 };
 
