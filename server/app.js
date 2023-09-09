6ccebb1b7c7b9e6221e45baad5b889c3f7a8b86b
Faheem Akhtar <faheemakhtar19730@gmail.com>
2023-09-09 16:17:30 +0530

    update

--- server/app.js
+++ server/app.js
@@ -7,12 +7,18 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser"); // Corrected the typo here
 const port = 4000;
 const bodyparser = require('body-parser');
+const path = require('path');
 
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser()); // Use cookieParser, not cookiParser
 app.use(cors());
 app.use(router);
+
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 const carRoutes = require('./routes/carRoutes');
 app.use('/api/cars', carRoutes);
